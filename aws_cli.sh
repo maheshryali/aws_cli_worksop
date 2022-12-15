e686822c3909332b09c0320f5bd7f4fa3c30f4e7
mahesh <maheshryali@gmail.com>
2022-12-15 14:41:11 +0530

    changes1

---
+++ aws_cli.sh
@@ -0,0 +1,11 @@
+## aws_cli ec2-describe-availabilityzones and creates a json file
+### 1. https://jsoncrack.com/editor
+aws ec2 describe-availability-zones > availabilityzones.json
+### To query the specific values
+aws ec2 describe-availability-zones --query "AvailabilityZones[0].ZoneId"
+### to find zone id for the zone name us-east-1a
+### for reference https://jmespath.org/tutorial.html#filter-projections 
+aws ec2 describe-availability-zones --query "AvailabilityZones[?ZoneName=='us-east-1a'].ZoneId | [0]" --output text
+### to see any service we have describe or list to view all 
+### To filter output for preference see scope,filter,jmespath
+### to repeat anything in shell scipt use while
---
+++ details.md
@@ -0,0 +1,4 @@
+For query the data we have object({}) and list ([])
+-1 means last value
+https://jmespath.org/tutorial.html
+https://jmespath.org/tutorial.html#filter-projections
